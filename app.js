3a6b50f6a2bd6ba7905120a96d380c7dad9048f0
Néo Abric <abricneo@gmail.com>
2022-03-16 11:45:09 +0100

    Initialisation de l'API

--- app.js
+++ app.js
@@ -6,4 +6,23 @@ app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
 
+/**/
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'dbuser',
+  password : 's3kreee7'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+/**/
+
 module.exports = app;
