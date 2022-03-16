90b6873a2ad6b92beaad844496384710427c89fc
Néo Abric <abricneo@gmail.com>
2022-03-16 10:51:09 +0100

    ajout de fichier et modification mineure

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
-module.export = app;
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
-const app = require('.app');
+const app = require('./app');
+
 app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
+
